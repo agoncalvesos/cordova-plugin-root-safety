074696398ec0a9be85f39d7dcc2e27a787893a96
André Gonçalves <andre.goncalves@outsystems.com>
2021-09-23 18:17:47 +0100

    Removed unneeded stuff and added build info feature

--- www/root-safety.js
+++ www/root-safety.js
@@ -9,23 +9,8 @@ var rootSafety = {
   attest: function(nonce, api_key, cb, err) {
     exec(cb, err, PLUGIN_NAME, "attest", [nonce, api_key]);
   },
-  offlineVerification: function(jws, cb, err) {
-    exec(cb, err, PLUGIN_NAME, "offlineVerify", [jws]);
-  },
-  onlineVerification: function(api_key, jws, cb, err) {
-    exec(cb, err, PLUGIN_NAME, "onlineVerify", [api_key, jws]);
-  },
-  extractPayload: function(jws, cb, err) {
-    exec(cb, err, PLUGIN_NAME, "extractPayload", [jws]);
-  },
-  checkAppVerification: function(cb, err) {
-    exec(cb, err, PLUGIN_NAME, "checkAppVerification", []);
-  },
-  listHarmfulApps: function(cb, err) {
-    exec(cb, err, PLUGIN_NAME, "listHarmfulApps", []);
-  },
-  enableAppVerification: function(cb, err) {
-    exec(cb, err, PLUGIN_NAME, "enableVerifyApps", []);
+  buildInfo: function(cb, err) {
+    exec(cb, err, PLUGIN_NAME, "buildInfo", []);
   }
 };
 
